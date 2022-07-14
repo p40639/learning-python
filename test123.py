bdf34a7a492f9013877be41d6fb62421db24234e
Sarah <p60322@gmail.com>
2022-07-13 23:29:37 -0700

    add index.html

--- test123.py
+++ test123.py
@@ -21,18 +21,32 @@ def split_list(lst,n):
 import numpy
 book = []
 for i in range(1,10,1):
+    line = ""
     for j in range(1,10,1):
         k = i*j
-        book.append("{k:2}".format(i=i, j=j, k=k))
-n=9
-newlst = list(split_list(book,n))
-newnewlst = numpy.array(newlst)
-print(newnewlst)
+        line += "{i:1}x{j:1}={k:2} ".format(i=i, j=j, k=k)
+    # print(line)
+# newlst = list(split_list(book,n))
+# newnewlst = numpy.array(newlst)
+# print(newnewlst)
 
 # for a in range(0,81,9):
 #     book1 = book[a:a+9:1]
 #     book2 = array([book2,book1])
 #     print(book2)
 
+def print_triangle(height: int) -> None:
+    width = height * 2 - 1
+    current_width = 1
+    mid = width // 2
+    for i in range(height):
+        line = ""
+        for j in range(width):
+            if (j > mid - current_width and j < mid + current_width):
+                line += "*"
+            else:
+                line += " "
+        current_width += 1
+        print(line)
 
-
+print_triangle(1)
