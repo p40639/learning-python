fe320ace38806b2cdc13be0a703903bc8380d26e
Sarah <p60322@gmail.com>
2022-07-16 16:28:50 -0700

    cs50 practise (before storing data)

--- hello.py
+++ hello.py
@@ -1,9 +1,24 @@
-from flask import Flask
-
+from flask import Flask, redirect, request
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    with open('index.html', 'r') as file:
+    with open('tingsweb-signup.html', 'r') as file:
+        data = file.read()
+        return data
+
+@app.route("/register")
+def register():
+    connection = sqlite3.connect('example.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * from users")
+    print(cursor.fetchall())
+    user = request.args.get("user")
+    password = request.args.get("password")
+    cursor.execute("INSERT INTO users VALUES (?,?,?,?,?)", ("Ting","email","123",user,password))
+    connection.commit()
+    connection.close()
+    with open('tingsweb.html', 'r') as file:
         data = file.read()
         return data
